a4b0e95756fd438ab4786d4329439d28d6cadb12
alfep <69330433+alfep@users.noreply.github.com>
2025-12-30 00:00:49 +0700

    fix syntax error index.js

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
 });
+
 
 db.connect((err) => {
   if (err) {
